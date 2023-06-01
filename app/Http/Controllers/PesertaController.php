c5e211b856695fa5178b36512fb62edadb763b5b
Arieumarella <al.razli.umarella.15111020@gmail.com>
2023-06-01 17:29:04 +0700

    hanya belum bisa edit

--- app/Http/Controllers/PesertaController.php
+++ app/Http/Controllers/PesertaController.php
@@ -32,6 +32,63 @@ class PesertaController extends Controller
 
         return redirect()
             ->route("peserta.index")
-            ->with(["success" => "Data Berhasil Disimpan.!"]);
+            ->with(["ok" => "Data Berhasil Disimpan.!"]);
+    }
+
+    public function edit($id = null)
+    {
+        $data = Peserta::find($id);
+
+        return response()->json([
+            "success" => true,
+            "data" => $data,
+        ]);
+    }
+
+    public function update($request, $id)
+    {
+        $this->validate($request, [
+            "nama" => "required|min:10",
+            "nomor_telefon" => "required|min:8|numeric",
+            "alamat" => "required|min:10",
+        ]);
+
+        $peserta = Peserta::findOrFail($id);
+
+        $peserta->update([
+            "nama" => $request->nama,
+            "nomor_telefon" => $request->nomor_telefon,
+            "alamat" => $request->alamat,
+        ]);
+
+        return redirect()
+            ->route("peserta.index")
+            ->with(["ok" => "Data Berhasil Disimpan.!"]);
+    }
+
+    public function show(Peserta $Peserta)
+    {
+        //return response
+        return response()->json([
+            "success" => true,
+            "message" => "Detail Data Post",
+            "data" => $Peserta,
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $pros = Peserta::where("id", $id)->delete();
+
+        if ($pros == true) {
+            session()->flash("ok", "Data berhasil dihapus.");
+        } else {
+            session()->flash("no", "Data gagal dihapus.");
+        }
+
+        return response()->json([
+            "success" => true,
+            "message" => "Data Post Berhasil Dihapus!.",
+        ]);
     }
 }
--- resources/views/peserta/index.blade.php
+++ resources/views/peserta/index.blade.php
@@ -7,6 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <title>Belajar laravel 10</title>
@@ -43,6 +45,20 @@
                         </div>
                       @endif
 
+                      @if(session('ok') != null)
+                        <div class="alert alert-success alert-dismissible fade show text-start" role="alert">
+                          {{ session('ok') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                      @endif
+
+                      @if(session('no') != null)
+                        <div class="alert alert-danger alert-dismissible fade show text-start" role="alert">
+                          {{ session('no') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                      @endif
+
                         <div class="text-end mt-2">
                             <button class="btn btn-primary" onclick="showAddModal()">Tambah Data</button>
                         </div>
@@ -53,6 +69,7 @@
                             <th scope="col">Nama</th>
                             <th scope="col">Kontak</th>
                             <th scope="col">Alamat</th>
+                            <th scope="col">Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -69,7 +86,11 @@
                                 <td>{{ $value->nama }}</td>
                                 <td>{{ $value->nomor_telefon }}</td>
                                 <td>{{ $value->alamat}}</td>
-                                </tr>
+                                <td>
+                                  <button class="btn btn-sm btn-warning" onclick="showModalEdit('{{ $value->id }}')"><i class="fa-solid fa-pen-to-square"></i></button>
+                                  <button class="btn btn-sm btn-danger" onclick="deleteData('{{ $value->id }}')"><i class="fa-solid fa-trash"></i></button>
+                                </td>
+                            </tr>
                             <tr>
                                 @php 
                             
@@ -136,11 +157,92 @@
         </div>
       </div>
 
+
+      {{-- Edit modal --}}
+      <div class="modal fade" id="modalEdit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal Edit Data</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form action="{{ route('peserta.update', '2') }}" id="formEdit" method="PUT">
+                @csrf
+                @method('PUT')
+                <div class="mb-3">
+                  <label for="recipient-name" class="col-form-label">Nama</label>
+                  <input type="text" class="form-control" name="nama" id="namaEdit" placeholder="input nama" required>
+                  <input type="hidden" class="form-control" name="idEdit" id="idEdit">
+                </div>
+                <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Telepon</label>
+                    <input type="text" class="form-control" name="nomor_telefon" id="nomor_telefonEdit" placeholder="input telepon" oninput="this.value = this.value.replace(/[^0-9]/g, '')" required>
+                  </div>
+                <div class="mb-3">
+                  <label for="message-text" class="col-form-label">Alamat</label>
+                  <textarea class="form-control" name="alamat" id="alamatEdit" placeholder="Input Alamat"></textarea>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+        </form>
+          </div>
+        </div>
+      </div>
+      {{-- End Edit Modal --}}
+
     <script>
     $( document ).ready(function() {
         showAddModal = function () {
             $('#modalTambah').modal('show');
         }
+
+        showModalEdit = function (id){
+
+          let token   = $("meta[name='csrf-token']").attr("content");
+
+          $.ajax({
+              url: "/peserta/"+id+"/edit",
+              type: "GET",
+              data: {
+                        "_token": token
+              },
+              success:function(res){    
+
+                $('#namaEdit').val(res.data.nama);
+                $('#nomor_telefonEdit').val(res.data.nomor_telefon);
+                $('#alamatEdit').val(res.data.alamat);
+                $('#idEdit').val(res.data.id);
+
+                $('#modalEdit').modal('show');
+
+              }
+          });
+
+        }
+
+
+        deleteData = function (id){
+
+          let token   = $("meta[name='csrf-token']").attr("content");
+
+          $.ajax({
+              url: "/peserta/"+id,
+              type: "DELETE",
+              data: {
+                        "_token": token
+              },
+              success:function(response){ 
+                location.reload();
+              }
+          });
+
+        }
+
+
     });
     </script>
   </body>
